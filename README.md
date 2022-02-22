c33fc797153ca60649429bf56c4be4efb361a415
Dipokal HHJ <hhj@devent.kr>
2022-02-22 13:18:25 +0900

    feat: create docker container
    + 도커 컨테이너 생성

--- README.md
+++ README.md
@@ -8,9 +8,11 @@
 * [x] 웹훅 받기
 * [x] 도커 연동
 * [x] 도커 - 깃허브 이미지 생성
-* [ ] 도커 컨테이너 생성
+* [x] 도커 컨테이너 생성
 * [x] git clone and download
 * [x] fix git clone event
+* [ ] 도커 컨테이너 이미지 태그 수정
+* [ ] 도커 컨테이너 버전관리
 
 
 
@@ -33,4 +35,14 @@ npm run start:prod
 
 ```
 npm run start:dev
+```
+
+
+## webhook
+
+
+**url**
+
+```
+https://test.test/api/webhook/github
 ```
--- backend/controllers/webhook.ctrl.js
+++ backend/controllers/webhook.ctrl.js
@@ -16,8 +16,8 @@ const event = new EventEmitter();
 const docker = new Dockerode(); 
 
 
-event.on('clone_repository', ()  => {
-    let url = 'https://github.com/alpinelinux/docker-alpine.git';
+event.on('clone_repository', (github_url)  => {
+    let url = github_url;
     let uuid = uuidv4();
 
     console.log(`[ + ] Clone '${url}' repository.`)
@@ -28,29 +28,49 @@ event.on('clone_repository', ()  => {
     })
 
     child.stdout.on('close', code => {
-        event.emit("create_container", uuid, url)
+        event.emit("create_image", uuid, url)
     });
 
 });
 
-event.on('create_container', (uuid, url) => {
+event.on('create_image', (uuid, url) => {
     console.log(`[ + ] Created '${uuid}' image.`)
+    let uuid_replaced = uuid.replaceAll('-','')
+    let image_tag = `${uuid_replaced}:2.12`
     docker.buildImage({
         context: `${clone_dir}/${uuid}`,
         src: ['Dockerfile']
-    }, {t: `${uuid.replaceAll('-','')}:2.12`, remote: url}, function (err, response) {
+    }, {t: image_tag, remote: url}, function (err, response) {
         //console.log(response)
+        event.emit("create_container", image_tag, uuid_replaced)
     });
 });
+
+event.on("create_container", (image_tag, uuid_replaced) => {
+    docker.createContainer({Image: image_tag, name: `${uuid_replaced}-test`}, function (err, container) {
+        container.start(function (err, data) {
+            console.log(`[ + ] Created '${uuid_replaced}' container.`)
+
+        });
+    });
+})
+
+
+
 
 export async function receiveWebhookFromGithub (req, res) {
     try {
         let webhook_data = req;
-
-        console.log(webhook_data)
+        let github_url = webhook_data.body.repository.clone_url
 
         let is_valid = await checkWebhookSecret(req)
         console.log(is_valid)
+
+        if (is_valid) {
+            event.emit("clone_repository", github_url)
+        } else {
+            return res.status(401).json({status:0})
+        }
 
     
         res.status(200).json({status:1})
@@ -62,7 +82,7 @@ export async function receiveWebhookFromGithub (req, res) {
 
 export function test (req, res) {
     try {
-        event.emit("clone_repository")
+        event.emit("clone_repository", "https://github.com/alpinelinux/docker-alpine.git")
 
     
         res.status(200).json({status:1})
